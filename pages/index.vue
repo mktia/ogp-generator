dfcc9bd1113b6d6d745b8cd90b6bc6ca06e28efe
mktia <19624453+mktia@users.noreply.github.com>
2020-11-03 03:29:03 +0900

    create base files

--- pages/index.vue
+++ pages/index.vue
@@ -1,93 +1,155 @@
-<template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>
-            Vuetify is a progressive Material Design component framework for
-            Vue.js. It was designed to empower developers to create amazing
-            applications.
-          </p>
-          <p>
-            For more information on Vuetify, check out the
-            <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation </a
-            >.
-          </p>
-          <p>
-            If you have questions, please join the official
-            <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord </a
-            >.
-          </p>
-          <p>
-            Find a bug? Report it on the github
-            <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board </a
-            >.
-          </p>
-          <p>
-            Thank you for developing with Vuetify and I look forward to bringing
-            more exciting features in the future.
-          </p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3" />
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br />
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+<template lang="pug">
+.index
+  v-row(justify='center', align='center')
+    v-col(cols='12', sm='10', md='8')
+      .description
+        p(v-for='sentence in $t("top_description")') {{ sentence }}
+  v-row(justify='center', align='center')
+    v-col(cols='12', sm='10', md='8')
+      p * {{ $t("form.attention") }}
+      v-form
+        v-container
+          v-text-field(
+            v-model='properties.pageTitle',
+            :rules='rules.title',
+            :label='"*" + this.$t("form.properties.page_title")'
+          )
+          v-text-field(
+            v-model='properties.siteName',
+            :label='this.$t("form.properties.site_name")',
+            required
+          )
+          v-text-field(
+            v-model='properties.description',
+            :label='this.$t("form.properties.description")'
+          )
+          v-text-field(
+            v-model='properties.url',
+            :rules='rules.url',
+            :label='"*" + this.$t("form.properties.url")',
+            required
+          )
+          v-text-field(
+            v-model='properties.imagePath',
+            :rules='rules.imagePath',
+            :label='"*" + this.$t("form.properties.image_path")',
+            required
+          )
+          v-select(
+            v-model='properties.contentType',
+            :items='this.$t("content_types")',
+            :rules='rules.contentType',
+            :label='"*" + this.$t("form.properties.content_type")',
+            required
+          )
+          v-text-field(
+            v-model='twitter.userName',
+            :label='$t("form.twitter.user_name")'
+          )
+          v-select(
+            v-model='twitter.cardType',
+            :items='this.$t("twitter_card_types")',
+            :label='this.$t("form.twitter.card_type")'
+          )
+      v-btn(@click='generateOgp') {{ $t('button') }}
+  v-row.mt-5(v-if='code != ""', justify='center', align='center')
+    v-col(cols='12', sm='10', md='8')
+      pre.ogp.pa-3
+        code {{ code }}
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
+  data() {
+    return {
+      properties: {
+        pageTitle: '',
+        siteName: '',
+        description: '',
+        url: '',
+        imagePath: '',
+        contentType: '',
+      },
+      twitter: {
+        userName: '',
+        cardType: '',
+      },
+      rules: {
+        title: [(v) => !!v || this.$t('form.rules.title.null')],
+        url: [
+          (v) => !!v || this.$t('form.rules.url.null'),
+          (v) => /https?:\/\//.test(v) || this.$t('form.rules.url.attention'),
+        ],
+        imagePath: [
+          (v) => !!v || this.$t('form.rules.image_path.null'),
+          (v) => /https?:\/\//.test(v) || this.$t('form.rules.url.attention'),
+        ],
+        contentType: [(v) => !!v || this.$t('form.rules.content_type.null')],
+      },
+      code: '<!-- Open Graph Protocol -->\n',
+    }
+  },
+  methods: {
+    generateOgp() {
+      if (this.properties.pageTitle !== '') {
+        this.code +=
+          `<meta property="og:title" content="${this.properties.pageTitle}" />` +
+          '\n'
+      }
+      if (this.properties.siteName !== '') {
+        this.code +=
+          `<meta property="og:site_name" content="${this.properties.siteName}" />` +
+          '\n'
+      }
+      if (this.properties.description !== '') {
+        this.code +=
+          `<meta property="og:description" content="${this.properties.description}" />` +
+          '\n'
+      }
+      if (this.properties.url !== '') {
+        this.code +=
+          `<meta property="og:url" content="${this.properties.url}" />` + '\n'
+      }
+      if (this.properties.imagePath !== '') {
+        this.code +=
+          `<meta property="og:image" content="${this.properties.imagePath}" />` +
+          '\n'
+      }
+      if (this.properties.contentType !== '') {
+        this.code +=
+          `<meta property="og:type" content="${this.properties.contentType}" />` +
+          '\n'
+      }
+      if (this.twitter.userName !== '') {
+        this.code +=
+          `<meta name="twitter:site" content="@${this.twitter.userName}" />` +
+          '\n' +
+          `<meta name="twitter:creator" content="@${this.twitter.userName}" />` +
+          '\n'
+      }
+      if (this.twitter.cardType !== '') {
+        this.code +=
+          `<meta name="twitter:card" content="${this.twitter.cardType}" />` +
+          '\n'
+      }
+    },
   },
 }
 </script>
+
+<style lang="sass" scoped>
+.description
+  text-align: justify
+
+.ogp
+  background-color: #fcfcfc
+  border: solid 1px #ccc
+  border-radius: 4px
+  font-family: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace
+  font-size: 1rem
+  font-weight: 500
+
+  code
+    background-color: transparent
+    color: #777
+</style>
