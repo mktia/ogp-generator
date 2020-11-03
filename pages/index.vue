35cb7fdd17d30abd4811ded059fa591a039d6971
mktia <19624453+mktia@users.noreply.github.com>
2020-11-03 18:34:31 +0900

    fix ads

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,9 @@
     v-col(cols='12', sm='10', md='8')
       .description
         p(v-for='sentence in $t("top_description")') {{ sentence }}
-  adsbygoogle(data-ad-slot='3806743874', data-ad-format='auto')
+  v-row(justify='center', align='center')
+    v-col(cols='12', sm='10', md='8')
+      adsbygoogle(data-ad-slot='3806743874', data-ad-format='auto')
   v-row(justify='center', align='center')
     v-col(cols='12', sm='10', md='8')
       p * {{ $t("form.attention") }}
@@ -59,7 +61,9 @@
             :label='this.$t("form.twitter.card_type")'
           )
       v-btn(@click='generateOgp') {{ $t("button") }}
-  adsbygoogle(data-ad-slot='3806743874', data-ad-format='auto')
+  v-row(justify='center', align='center')
+    v-col(cols='12', sm='10', md='8')
+      adsbygoogle(data-ad-slot='3806743874', data-ad-format='auto')
   v-row.mt-5(v-if='code != ""', justify='center', align='center')
     v-col(cols='12', sm='10', md='8')
       p.description {{ $t("about_prefix") }}
