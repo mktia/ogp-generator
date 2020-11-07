dea0d246087cdb0fea46849e748b06b9c23a186e
mktia <19624453+mktia@users.noreply.github.com>
2020-11-08 02:13:13 +0900

    add dark mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,6 +4,8 @@ v-app(light)
     v-toolbar-title {{ $t("title") }}
     v-spacer
     v-btn(icon)
+      v-icon(@click.stop='isDark = !isDark') {{ themeIcon }}
+    v-btn(icon)
       v-icon(@click.stop='rightDrawer = !rightDrawer') mdi-translate
   v-main
     v-container
@@ -26,6 +28,7 @@ v-app(light)
 export default {
   data() {
     return {
+      isDark: false,
       right: true,
       rightDrawer: false,
     }
@@ -33,6 +36,10 @@ export default {
   computed: {
     availableLocales() {
       return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    },
+    themeIcon() {
+      this.$vuetify.theme.dark = this.isDark
+      return this.isDark ? 'mdi-brightness-4' : 'mdi-brightness-6'
     },
   },
 }
